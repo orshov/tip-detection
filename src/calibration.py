45cacd7eb6a6d134e69d43de90bc7c8d33b2317d
Or Shoval <orshov.tech@gmail.com>
2026-03-16 11:55:06 +0200

    Add Calibration class in src/calibration.py

---
+++ src/calibration.py
@@ -0,0 +1,11 @@
+class Calibration:
+    def __init__(self):
+        pass
+
+    def calibrate(self):
+        # Calibration logic goes here
+        pass
+
+    def get_calibration_data(self):
+        # Return calibration data
+        return None
